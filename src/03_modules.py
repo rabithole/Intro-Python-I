aa060b1d672b101bdf6d5f2acd9f99d07759f0ab
Robert Kulp <pedalinon@gmail.com>
2020-09-02 19:48:05 -0700

    Printing is done

--- src/03_modules.py
+++ src/03_modules.py
@@ -11,21 +11,65 @@ import sys
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
+# x = dir(sys.argv)
+
+# for (index, element) in enumerate(x):
+# 	print(f'Element {index} is {element}')
+
+# total arguments 
+# n = len(sys.argv) 
+# print("Total arguments passed:", n) 
+  
+# # Arguments passed 
+# print("\nName of Python script:", sys.argv[0]) 
+  
+# print("\nArguments passed:", end = " ") 
+# for i in range(1, n): 
+#     print(sys.argv[i], end = " ") 
+      
+# # Addition of numbers 
+# Sum = 0
+# # Using argparse module 
+# for i in range(1, n): 
+#     Sum += int(sys.argv[i]) 
+      
+# print("\n\nResult:", Sum) 
+
+# if __name__ == "__main__":
+#     print(f"Arguments count: {len(sys.argv)}")
+#     for i, arg in enumerate(sys.argv):
+#         print(f"Argument {i:>6}: {arg}")
+
+
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+import os
+import platform
+
+print(os.name)
+print(sys.platform)
+print(platform.system())
+print(platform.release())
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+print('')
+print(sys.version)
 
-
-import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
-
 # Print the current process ID
 # YOUR CODE HERE
+print('')
+print(os.getpid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print('')
+print(os.getcwd())
 
 # Print out your machine's login name
 # YOUR CODE HERE
+print('')
+print(os.getlogin())
+
+
